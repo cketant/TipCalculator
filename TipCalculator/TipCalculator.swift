4b7ca91d10ff4cf929f94379dd80d3e7266c5375
Chris Ketant <cketant@apple.com>
2016-09-17 02:02:14 -0700

    Integrated dynamic functionality. Functioning calculator

--- TipCalculator/TipCalculator.swift
+++ TipCalculator/TipCalculator.swift
@@ -11,6 +11,8 @@ import UIKit
 class TipCalculator: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var segment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +24,19 @@ class TipCalculator: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func calcTip(_ sender: AnyObject) {
-        tipLabel.text = "$10.00"
-        totalLabel.text = "$110.00"
+    @IBAction func billChange(_ sender: AnyObject) {
+        let tipPercents     = [0.18, 0.2, 0.25]
+        let bill            = Double(billField.text!) ?? 0
+        let tip             = bill * tipPercents[segment.selectedSegmentIndex]
+        let total           = bill + tip
+        
+        tipLabel.text       = String(format: "$%.2f", tip)
+        totalLabel.text     = String(format: "$%.2f", total)
     }
 
     @IBAction func onTap(_ sender: AnyObject) {
-        view.endEditing(true)
+        self.view!.endEditing(true)
     }
+
 }
 
