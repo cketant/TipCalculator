06367979fa26f2279e44af0522ceb891f58d4fd4
Chris Ketant <cketant@apple.com>
2016-09-17 17:18:14 -0700

    Added keyboard always visible

--- TipCalculator/TipCalculator.swift
+++ TipCalculator/TipCalculator.swift
@@ -15,8 +15,15 @@ class TipCalculator: UIViewController {
     @IBOutlet weak var totalTitleLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var segment: UISegmentedControl!
-    @IBOutlet weak var billTitleLabel: UILabel!
+    @IBOutlet weak var fieldView: UIView!
+    var fieldOriginalY: CGFloat!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(TipCalculator.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TipCalculator.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let defaults = UserDefaults.standard
@@ -35,8 +42,31 @@ class TipCalculator: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.animateNavBar()
+        self.billField.becomeFirstResponder()
     }
 
+    func keyboardWillShow(){
+        let x = self.fieldView.frame.origin.x
+        self.fieldOriginalY = self.fieldView.frame.origin.y
+        let width = self.fieldView.frame.size.width
+        let height = self.fieldView.frame.size.height
+        let navBarHeight = self.navigationController?.navigationBar.frame.height
+        UIView.animate(withDuration: 0.2) { 
+            self.fieldView.frame = CGRect.init(x: x, y: navBarHeight!, width: width, height: height)
+            self.showViewElements(isHidden: false)
+        }
+    }
+    
+    func keyboardWillHide(){
+        let x = self.fieldView.frame.origin.x
+        let width = self.fieldView.frame.size.width
+        let height = self.fieldView.frame.size.height
+        UIView.animate(withDuration: 0.2) {
+            self.fieldView.frame = CGRect.init(x: x, y: self.fieldOriginalY, width: width, height: height)
+            self.showViewElements(isHidden: true)
+        }
+    }
+    
     @IBAction func billChange(_ sender: AnyObject) {
         let tipPercents     = [0.18, 0.2, 0.25]
         let bill            = Double(billField.text!) ?? 0
@@ -56,6 +86,22 @@ class TipCalculator: UIViewController {
 
     @IBAction func onTap(_ sender: AnyObject) {
         self.view!.endEditing(true)
+    }
+    
+    func showViewElements(isHidden: Bool) {
+        if isHidden {
+            self.segment.isHidden = true
+            self.tipLabel.isHidden = true
+            self.tipTitleLabel.isHidden = true
+            self.totalLabel.isHidden = true
+            self.totalTitleLabel.isHidden = true
+        }else {
+            self.segment.isHidden = false
+            self.tipLabel.isHidden = false
+            self.tipTitleLabel.isHidden = false
+            self.totalLabel.isHidden = false
+            self.totalTitleLabel.isHidden = false
+        }
     }
     
     func animateNavBar() {
@@ -80,20 +126,24 @@ class TipCalculator: UIViewController {
     func animateBackground(isLight: Bool){
         if isLight {
             self.view.backgroundColor = UIColor.white
-            self.billTitleLabel.textColor = UIColor.black
+            self.fieldView.backgroundColor = UIColor.white
             self.tipTitleLabel.textColor = UIColor.black
             self.totalTitleLabel.textColor = UIColor.black
-//            self.billField.textColor = UIColor.black
             self.tipLabel.textColor = UIColor.black
             self.totalLabel.textColor = UIColor.black
+            self.billField.backgroundColor = UIColor.white
+            self.billField.textColor = UIColor.black
+            self.segment.tintColor = UIColor.black
         }else {
             self.view.backgroundColor = UIColor.black
-            self.billTitleLabel.textColor = UIColor.white
+            self.fieldView.backgroundColor = UIColor.black
             self.tipTitleLabel.textColor = UIColor.white
             self.totalTitleLabel.textColor = UIColor.white
-//            self.billField.textColor = UIColor.white
             self.tipLabel.textColor = UIColor.white
             self.totalLabel.textColor = UIColor.white
+            self.billField.backgroundColor = UIColor.black
+            self.billField.textColor = UIColor.white
+            self.segment.tintColor = UIColor.white
         }
     
     }
